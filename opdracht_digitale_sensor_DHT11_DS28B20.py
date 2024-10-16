7f91e7423c472ec2d4852b6ca25350c41376df32
cetje0 <voce140307@leerling.mosa-rt.be>
2024-10-16 10:21:19 +0200

    added opdracht

---
+++ opdracht_digitale_sensor_DHT11_DS28B20.py
@@ -0,0 +1,33 @@
+import time
+from w1thermsensor import W1ThermSensor
+import board
+import adafruit_dht
+import gpiozero as io
+
+dht11 = adafruit_dht.DHT11(board.D18)
+sensor=W1ThermSensor() # sensor pin op 4
+var_LED_rood = io.LED(26)
+f = open("gemiddelde_temperatuur.txt", "w")
+gemiddelde = 0
+
+while True:
+    DS18B20_c = round(sensor.get_temperature(), 0)
+    temp_dht11 = dht11.temperature
+
+    print(f"temperatuur van DS18B20 {DS18B20_c}")
+    print(f"temperatuur van DHT11 {temp_dht11}")
+
+    gemiddelde = (DS18B20_c + temp_dht11) /2
+
+
+    if (abs(DS18B20_c - temp_dht11 > 5)):
+        var_LED_rood.on()
+        f = open("gemiddelde_temperatuur.txt", "a")
+        f.write(f"temperatuurverschil is te hoog, check de sensor.\n")
+        f.close()
+    else:
+        var_LED_rood.off()
+        f = open("gemiddelde_temperatuur.txt", "a")
+        f.write(f"gemiddelde temp is: {gemiddelde}\n")
+        f.close()
+    time.sleep(1)
