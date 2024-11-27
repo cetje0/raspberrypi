b1be113b4bbfef08f71021439086faeb76109595
cetje0 <voce140307@leerling.mosa-rt.be>
2024-11-27 10:18:32 +0100

    added

---
+++ DHT_CSVfile.py
@@ -0,0 +1,17 @@
+import board
+import adafruit_dht 
+from time import sleep, strftime
+
+dht11_1 = adafruit_dht.DHT11(board.D18)
+dht11_2 = adafruit_dht.DHT11(board.D23)
+
+with open("eggs.csv", "a") as csvfile:
+    while True:
+        sleep(3)
+        try:
+            temp_dht11_1 = dht11_1.temperature
+            temp_dht11_2 = dht11_2.temperature
+        except RuntimeError as error:
+            print(error.args[0])
+            continue
+        csvfile.write("{0}, {1}, {2}\n".format(strftime("%Y-%m-%d %H:%M:%s"), temp_dht11_1, temp_dht11_2))
