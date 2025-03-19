17c581fd84824800cd48b464bb3e80750430ed3e
cetje0 <voce140307@leerling.mosa-rt.be>
2025-03-19 10:37:51 +0100

    modified

--- stappen_motor.py
+++ stappen_motor.py
@@ -1,12 +1,12 @@
 import gpiozero as io
 from RpiMotorLib import RpiMotorLib
-
-afstand_in_mm = int(input(f"Welke afstand wiltu afleggen(mm) ?"))
-stappen_motor = RpiMotorLib.BYJMotor("BYJmotorX", "28BYJ")
-stappen = abs((afstand_in_mm / 7)*512)
-stap_motor_pins = [18,23,24,25]
-if afstand_in_mm < 0:
-    stappen_motor.motor_run(stap_motor_pins, 0.005,stappen,True,False,"full", 0.05)
-else:
-    stappen_motor.motor_run(stap_motor_pins, 0.005, stappen, False, False,"full",0.05)
+while True:
+    afstand_in_mm = int(input(f"Welke afstand wiltu afleggen(mm) ?"))
+    stappen_motor = RpiMotorLib.BYJMotor("BYJmotorX", "28BYJ")
+    stappen = abs((afstand_in_mm / 7)*512)
+    stap_motor_pins = [18,23,24,25]
+    if afstand_in_mm < 0:
+        stappen_motor.motor_run(stap_motor_pins, 0.003,stappen,True,False,"full", 0.05)
+    else:
+        stappen_motor.motor_run(stap_motor_pins, 0.003, stappen, False, False,"full",0.05)
 
