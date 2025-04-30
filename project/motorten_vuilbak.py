9eda571f383a190b01c2d9cdd42760d0894645bc
cetje0 <voce140307@leerling.mosa-rt.be>
2025-04-30 09:34:57 +0200

    added

--- project/motorten_vuilbak.py
+++ project/motorten_vuilbak.py
@@ -1,24 +1,31 @@
 import gpiozero as io
 from time import sleep
-uitkomst = ""
-servo_draaien = io.AngularServo(17, min_angle = 0, max_angle = 360)
-servo_duwen = io.AngularServo(21, min_angle = 0, max_angle = 360)
-while True:
-    if uitkomst == "rest":
+uitkomst = "pmd"
+servo_draaien = io.AngularServo(20, min_angle = -90, max_angle = 90)
+servo_duwen = io.AngularServo(16, min_angle = 0, max_angle = 180)
+""" " ""   if uitkomst == "'rest'":
         servo_draaien.angle = 0
+        print("okay")
         sleep(1)
         servo_duwen.angle = 360
         sleep(0.5)
         servo_duwen.angle = 0
-    elif uitkomst == "pmd":
-        servo_draaien.angle = 90
-        sleep(1)
-        servo_duwen.angle = 360
-        sleep(0.5)
-        servo_duwen.angle = 0
-    elif uitkomst == "gft":
+            elif uitkomst == "gft":
         servo_draaien.angle = 180
+        print("okay")
         sleep(1)
         servo_duwen.angle = 360
         sleep(0.5)
-        servo_duwen.angle = 0
+        servo_duwen.angle = 0"""""" """
+while True:
+    servo_draaien.angle = -90
+    sleep(2)
+    servo_draaien.angle = -45
+    sleep(2)
+    servo_draaien.angle = 0
+    sleep(2)
+    servo_draaien.angle = 45
+    sleep(2)
+    servo_draaien.angle = 90
+    sleep(2)
+
